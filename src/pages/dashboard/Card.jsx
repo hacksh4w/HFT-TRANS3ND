92aeb34afeb19bc646280f2a6c1e4fc7e5bab06c
subru-37 <easwaransubramaniOO7@gmail.com>
2022-12-18 03:59:32 +0530

    dashboard pages

--- src/pages/dashboard/Card.jsx
+++ src/pages/dashboard/Card.jsx
@@ -1,10 +1,39 @@
 import React from 'react'
-import carp from '../../assets/carp.svg'
-export default function Card() {
-  return (
-    <div style={{height: '350px', width:'250px'}}>
-        <img style={{width:'100%'}} src={carp}></img>
+import carp from '../../assets/carp.svg';
+import './Card.css';
+const Card = (props) => {
+      return (
+        <div style={{ height: '300px', width: '250px',padding:'20px' }} className='parent'>
+        <style> @import url('https://fonts.googleapis.com/css2?family=Staatliches&display=swap') </style>
+        <div style={{
+            backgroundImage: `url(${carp})`,
+            width: '100%',
+            height: '30%',
+            backgroundRepeat: 'no-repeat',
+            backgroundSize: 'cover',
+            display: 'flex',
+            justifyContent: 'flex-end',
+            flexDirection: 'column'
+        }}>
+            <p style={{ fontFamily: "'Staatliches', cursive", 
+                        letterSpacing: '0.03em', 
+                        fontWeight: '400',
+                        fontSize: '37px', 
+                        lineHeight: '60px', 
+                        color: '#E39B06', 
+                        padding: '5px' }}>{props.job}</p>
+        </div>
+        <div style={{ display: 'flex', flexDirection: 'row' }} className='a1'>
+            <div style={{ display: 'flex', flexDirection: 'column', maxWidth: '50%' }}>
+                <p className='Jobdes'>Job Description</p>
+                <p className='des'> lorem ipsum asadasfasfasf f afasfasfasfasfa a shgn h hnhgnhn  hgnghn ghmghm </p>
+            </div>
+            <div style={{ display: 'flex', flexDirection: 'column', maxWidth: '50%' }}>
 
+            </div>
+        </div>
     </div>
-  )
+      );
 }
+
+export default Card;
