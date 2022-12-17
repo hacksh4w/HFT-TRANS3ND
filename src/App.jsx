63e0e2ce8708dd0cf25a8f3c6c2f64e87ac74ec7
subru-37 <easwaransubramaniOO7@gmail.com>
2022-12-17 12:41:03 +0530

    added navigation bar

--- src/App.jsx
+++ src/App.jsx
@@ -3,18 +3,18 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
 import ErrorPage from './error-page';
 import Landing from './pages/Landing/Landing';
+import { Navbar } from './components/navbar/Navbar.jsx';
 export default function App(){
   
   return (
-    <div className="App" style={{backgroundColor: theme ? "black" :"#FFE15D"}} >
-      <Router>  
-      {/*<Navbar />*/}
+    // style={{backgroundColor: theme ? "black" :"#FFE15D"}}
+    <div className="App" > 
+    <Navbar/> 
         <Routes>
           <Route path="/" element={<Landing/>} errorElement={<ErrorPage/>} />
           {/* <Route path="/about" element={<AboutPage />}/>
           <Route path="/dashboard" element={<DashBoard />}/> */}
         </Routes>
-      </Router>
     </div>
   )
 }
