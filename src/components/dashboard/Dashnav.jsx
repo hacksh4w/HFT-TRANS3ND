c6a14cce3ffaf260c5f2666e77f7675217c3a860
subru-37 <easwaransubramaniOO7@gmail.com>
2022-12-18 00:35:47 +0530

    betterment??! :_(

--- src/components/dashboard/Dashnav.jsx
+++ src/components/dashboard/Dashnav.jsx
@@ -6,17 +6,26 @@ import WorkOutlineIcon from '@mui/icons-material/WorkOutline';
 import SearchIcon from '@mui/icons-material/Search';
 import { Link } from 'react-router-dom';
 import './Dashnav.css'
+import HomeIcon from '@mui/icons-material/Home';
 export default function () {
   return (
-    <div style={{   maxWidth: '30vw', 
+    <div style={{   width: '30vw', 
+                    backgroundColor:'#9A0505',
                     minHeight:'100vh',
                     display:'flex',
                     alignItems:'center',
                     justifyContent:'flex-end',
-                    flexDirection:'column'
+                    flexDirection:'column',
+                    borderTopRightRadius:'20px',
+                    borderBottomRightRadius:'20px'
                     }}>
+        <Link to='/'><HomeIcon sx={{fontSize:'50px',position:'absolute',top:'20px',left:'20px',color:'white',
+            '&:hover':{
+                color: '#E39B06'
+            }}}/></Link>
         <div style={{
         backgroundColor:'white', 
+        borderRadius:'20px',
         width:'80%',
         minHeight:'75vh',
         marginBottom: '50px',
@@ -27,19 +36,19 @@ export default function () {
             <img style={{height: '150px',position:'absolute',top: '75px',left:'150px'}} src={dp}></img>
             <div style={{display:'flex',flexDirection:'column'}}>
                 <div className='flex'>
-                    <div style={{height:'100%',width:'30%',paddingLeft:'20px'}}><AppsIcon sx={{fontSize:'50px'}}/></div>
+                    <div className='flex' style={{height:'100%',width:'30%',paddingLeft:'20px'}}><AppsIcon sx={{fontSize:'50px'}}/></div>
                     <Link to='/mainpage' style={{all:'unset',width:'70%'}}><div style={{height:'100%'}}><h1 style={{margin: '20px'}}>Dashboard</h1></div></Link>
                 </div>
                 <div className='flex'>
-                    <div style={{height:'100%',width:'30%',paddingLeft:'20px'}}><PersonIcon sx={{fontSize:'50px'}}/></div>
+                    <div className='flex' style={{height:'100%',width:'30%',paddingLeft:'20px'}}><PersonIcon sx={{fontSize:'50px'}}/></div>
                     <Link to='/mainpage' style={{all:'unset',width:'70%'}}><div style={{height:'100%'}}><h1 style={{margin: '20px'}}>Profile</h1></div></Link>
                 </div>
                 <div className='flex'>
-                    <div style={{height:'100%',width:'30%',paddingLeft:'20px'}}><WorkOutlineIcon sx={{fontSize:'50px'}}/></div>
+                    <div className='flex' style={{height:'100%',width:'30%',paddingLeft:'20px'}}><WorkOutlineIcon sx={{fontSize:'50px'}}/></div>
                     <Link to='/mainpage' style={{all:'unset',width:'70%'}}><div style={{height:'100%'}}><h1 style={{margin: '20px'}}>Request A Job</h1></div></Link>
                 </div>
                 <div className='flex'>
-                    <div style={{height:'100%',width:'30%',paddingLeft:'20px'}}><SearchIcon sx={{fontSize:'50px'}}/></div>
+                    <div className='flex' style={{height:'100%',width:'30%',paddingLeft:'20px'}}><SearchIcon sx={{fontSize:'50px'}}/></div>
                     <Link to='/mainpage' style={{all:'unset',width:'70%'}}><div style={{height:'100%'}}><h1 style={{margin: '20px'}}>Categories</h1></div></Link>
                 </div>
             </div>
--- src/pages/mainpage/Mainpage.jsx
+++ src/pages/mainpage/Mainpage.jsx
@@ -1,9 +1,67 @@
-import React from 'react'
-import Dashnav from '../../components/dashboard/Dashnav'
+import React,{useState} from 'react';
+import Dashnav from '../../components/dashboard/Dashnav';
+import { TextField } from '@mui/material';
+import {InputAdornment} from '@mui/material';
+import searchIcon from '../../assets/frame71.svg'
 export default function Mainpage() {
+  const [value,setValue] = useState('');
+  const [search, setSearch] = useState(true);
+  function handleChange(event){
+    setValue((preValue)=>{
+        return{
+            ...preValue,
+            value
+        }
+    })
+    console.log(value)
+  }
   return (
-    <div style={{backgroundColor: '#9A0505', minHeight:'100vh'}}>
+    <div style={{backgroundColor: 'white', minHeight:'100vh',display:'flex',flexDirection:'row'}}>
         <Dashnav/>
+        <div style={{width:'70vw'}}>
+        <button onClick={()=>(setSearch(!search))} style={{background:'transparent',border:'none',display: search ? 'none' : 'flex',padding:'30px',position:'relative',left:'9.5px'}}>
+                                    <img style={{height:'55px'}} src={searchIcon}></img>
+                            </button>
+        <TextField 
+                name='Name'
+                autoComplete='off'
+                onChange={handleChange} 
+                sx={{
+                    width: '90%',
+                    paddingTop:'30px',
+                    paddingLeft:'60px',
+                    display: search ? 'flex' : 'none',
+                    transition: '1s',
+                    '& .MuiOutlinedInput-root':{
+                        borderRadius:'20px',
+                        background: 'linear-gradient(0deg, rgba(0, 0, 0, 0.1), rgba(0, 0, 0, 0.1)), #FFFFFF;',
+                      '& fieldset':{
+                        borderColor: 'white'
+                      },
+                      '&.Mui-focused fieldset': {
+                        borderColor: 'white'
+                      },
+                    },
+                    '& .MuiOutlinedInput-root:hover':{
+                      '& fieldset':{
+                        borderColor:'white'
+                      }
+                    }
+                    }} 
+                InputProps={{startAdornment: (
+                            <InputAdornment position="start">
+                            <button onClick={()=>(setSearch(!search))} style={{background:'transparent',border:'none',display: search ? 'flex' : 'none',position:'relative',right:'35px',zIndex:'10000000'}}>
+                                    <img style={{height:'55px'}} src={searchIcon}></img>
+                            </button>
+                            </InputAdornment>),
+                            style: {color:'black'}}}
+                InputLabelProps={{
+                  style: { color:'white' },
+                }}
+                variant='outlined'
+                label=''
+                id="outlined-basic"/>
+        </div>
     </div>
   )
 }
