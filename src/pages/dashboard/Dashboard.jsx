32f80b8fa252f2dcb72673223310b78893e6574a
subru-37 <easwaransubramaniOO7@gmail.com>
2022-12-18 10:29:30 +0530

    minor dashboard changes

--- src/pages/dashboard/Dashboard.jsx
+++ src/pages/dashboard/Dashboard.jsx
@@ -3,12 +3,12 @@ import Card from './Card';
 export default function Dashboard() {
   return (
     <div style={{display:'flex',flexGrow: '1',flexDirection:'row',flexWrap:'wrap',alignItems:"center",justifyContent:'center',paddingTop:'40px',width:'75%'}}>
-        <Card job='Carpenter'/>
-        <Card job='Carpenter'/>
-        <Card job='Carpenter'/>
-        <Card job='Carpenter'/>
-        <Card job='Carpenter'/>
-        <Card job='Carpenter'/>
+        <Card job='Carpenter' username='hacksh4w'/>
+        <Card job='Plumber' username='hacksh4w'/>
+        <Card job='Electrician' username='hacksh4w'/>
+        <Card job='Gardner' username='hacksh4w'/>
+        <Card job='Nurse' username='hacksh4w'/>
+        <Card job='Carpenter' username='hacksh4w'/>
     </div>
   )
 }
